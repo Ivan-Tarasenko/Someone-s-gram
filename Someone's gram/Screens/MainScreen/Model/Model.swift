63bec586e0575e2be2447eb0548d8e2d58e64000
Ivan-Tarasenko <80888936+Ivan-Tarasenko@users.noreply.github.com>
2025-03-17 22:18:51 +0300

    The first commit assembled the architecture and connected Alamofire

---
+++ Someone's gram/Screens/MainScreen/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Someone's gram
+//
+//  Created by Иван Тарасенко on 17.03.2025.
+//
+
+import Foundation
---
+++ Someone's gram/Services/Network/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  Someone's gram
+//
+//  Created by Иван Тарасенко on 17.03.2025.
+//
+
+import Foundation
