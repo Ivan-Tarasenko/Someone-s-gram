208b6eedc17ec44a633381f2a3ec84c972984f11
Ivan-Tarasenko <80888936+Ivan-Tarasenko@users.noreply.github.com>
2025-03-17 21:40:08 +0300

    Initial Commit

---
+++ Someone's gram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Someone's gram
+//
+//  Created by Иван Тарасенко on 17.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
